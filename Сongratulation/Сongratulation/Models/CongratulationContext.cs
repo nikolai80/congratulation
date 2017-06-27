21157cb6a35b5a8ef355bcf2cc846ea13db7d26a
NCPI\Rybchenko <Rybchenko@ncpi.gov.by>
2017-06-27 16:50:20 +0300

    add CongratulationUser model class and dbContext class

---
+++ Сongratulation/Сongratulation/Models/CongratulationContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Сongratulation.Models
+{
+    public class CongratulationContext:DbContext
+    {
+    public DbSet<СongratulateUser> СongratulateUsers { get; set; }
+
+    public CongratulationContext(DbContextOptions<CongratulationContext> options) : base(options) { }
+
+  }
+}
