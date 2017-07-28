dcf1cb48bd8a2cadcea0eb9a7a9350de222c434b
NCPI\Rybchenko <Rybchenko@ncpi.gov.by>
2017-07-28 15:01:00 +0300

    Create DTO for user and service for UserDto

---
+++ congratulation/congratulation/BLL/DTO/UserDto.cs
@@ -0,0 +1,19 @@
+﻿using congratulation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace congratulation.BLL.DTO
+{
+  public class UserDto
+  {
+
+    public int Id { get; set; }
+    public string Alias { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public System.DateTime BirthdayDate { get; set; }
+    public virtual ICollection<uc_CongratulationCard> uc_CongratulationCard { get; set; }
+  }
+}
---
+++ congratulation/congratulation/BLL/DTO/UserService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using congratulation.Dal;
+using AutoMapper;
+using congratulation.Models;
+
+namespace congratulation.BLL.DTO
+{
+  public class UserService : IUserService
+  {
+    UnitOfWork Db { get; set; }
+
+    public UserService(UnitOfWork database)
+    {
+      Db = database;
+    }
+    public void Dispose()
+    {
+      Db.Dispose();
+    }
+
+    public IEnumerable<UserDto> GetAllUsers()
+    {
+      Mapper.Initialize(cfg => cfg.CreateMap<uc_User, UserDto>());
+      return Mapper.Map<IEnumerable<uc_User>, List<UserDto>>(Db.UserRepository.GetAll());
+    }
+
+    public UserDto GetUser(int? id)
+    {
+      UserDto res = null;
+      if (id==null)
+      {
+        throw new ArgumentException("id не может быть null");
+      }
+      var user = Db.UserRepository.Get(id.Value);
+      if (user==null)
+      {
+        throw new Exception("Пользхователь не найден");
+      }
+      else
+      {
+        Mapper.Initialize(cfg => cfg.CreateMap<uc_User, UserDto>());
+        res = Mapper.Map<uc_User, UserDto>(user);
+      }
+      return res;
+    }
+  }
+}
